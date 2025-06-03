a02f4e2ed02552c870e917d7cb35fec797ecf9a8
Curtis Ly <137116222+lycurtis@users.noreply.github.com>
2025-06-02 22:10:19 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,50 +1,29 @@
-/* Ethan Le ele038@ucr.edu
-* Discussion Section: 022
-* Assignment: Custom Laboratory Project Demo Video #1
-* Exercise Description:  
-* 
-* 
-* I acknowledge all content contained herein, excluding template or example code, is my own original work.
-*
-* Demo Link: https://www.youtube.com/watch?v=TqTUJ_ag7Lw
-
-*
-*/
 #include "timerISR.h"
 #include "helper.h"
 #include "spiAVR.h"
 #include "irAVR.h"
-#include "LCD.h"
+//#include "LCD.h"
 #include "periph.h"
-#include "serialATmega.h"
-
-
-
-// LED
-// SCK(SCL) = PB1
-// SDA(MOSI) = PB2
-// AO = PH3
-// Reset = PG5
-// CS(SS) = PE5
-// GND
-// VCC
-#define PIN_A0 PORTH3
-#define PIN_RESET PORTG5
-#define PIN_CS PORTE5
-
-#define SWRESET 0x01
-#define SLPOUT 0x11
-#define COLMOD 0x3A
-#define DISPON 0x29
-#define CASET 0x2A
-#define RASET 0x2B
-#define RAMWR 0x2C
-
-
-
-
+//#include "serialATmega.h"
 
 //128*160
+// LED
+// PIN_SCK PORTB1
+// PIN_MOSI PORTB2
+// PIN_A0 PORTH3
+// PIN_RESET PORTG5
+// PIN_CS PORTE5
+// GND
+// VCC
+#define SWRESET 0x01 //Software RESET
+#define SLPOUT 0x11 //Sleep out
+#define COLMOD 0x3A //Interface Pixel Format
+#define DISPON 0x29 //Display On
+#define CASET 0x2A //Column Address Set
+#define RASET 0x2B //Row Address Set
+#define RAMWR 0x2C //Memory Write
+
+unsigned short p1Joy;
 
 void HardwareReset()
 {
@@ -55,30 +34,26 @@ void HardwareReset()
   _delay_ms(200);
 }
 
-
-
 void Send_Command(char cmd)
 {
-  PORTH &= ~(1 << PIN_A0);
-  PORTE &= ~(1 << PIN_CS);
+  PORTH &= ~(1 << PIN_A0); //A0 for low command
+  PORTE &= ~(1 << PIN_CS); //CS low to select TFT
   SPI_SEND(cmd);
-  PORTE |= (1 << PIN_CS);
+  PORTE |= (1 << PIN_CS); //CS high to deselect
 }
 
 void Send_Data(char data)
 {
-  PORTH |= (1 << PIN_A0);
-  PORTE &= ~(1 << PIN_CS);
+  PORTH |= (1 << PIN_A0); //A0 high for data
+  PORTE &= ~(1 << PIN_CS); //CS low to select TFT
   SPI_SEND(data);
-  PORTE |= (1 << PIN_CS);
+  PORTE |= (1 << PIN_CS); //CS high to deselct 
 }
-
-
 
 void ST7735_init()
 {
   HardwareReset();
-
+  
   Send_Command(SWRESET);
   _delay_ms(150);
 
@@ -86,47 +61,34 @@ void ST7735_init()
   _delay_ms(200);
 
   Send_Command(COLMOD);
-  Send_Data(0x05);
+  Send_Data(0x05); //set 16 bit color mode
   _delay_ms(10);
 
   Send_Command(DISPON);
   _delay_ms(200);
-
 }
-
 
 //row-horizontal (128)
 //columns-vertical (160)
-void blackbackGround(uint16_t color)
+void fillScreen(uint16_t topColor, uint16_t bottomColor)
 {
-  //column
-  Send_Command(CASET);
-  Send_Data(0x00);
-  Send_Data(0x00); //starts at 0
-  Send_Data(0x00);
-  Send_Data(0x7F); //ends at 127
+  Send_Command(CASET); //set column address
+  Send_Data(0x00); Send_Data(0x00); //start at 0
+  Send_Data(0x00); Send_Data(0x7F); // end at 127
 
-  //row
-  Send_Command(RASET);
-  Send_Data(0x00);
-  Send_Data(0x00); //starts at 0
-  Send_Data(0x00);
-  Send_Data(0x9F); //ends at 159
+  Send_Command(RASET); //set row address
+  Send_Data(0x00); Send_Data(0x00); //start at 0
+  Send_Data(0x00); Send_Data(0x9F); // end at 159
 
   Send_Command(RAMWR); //write RAM
 
-  for (int i = 0; i < 128 * 160; ++i)
-  {
-    Send_Data (color >> 8);
-    Send_Data (color & 0xFF);  
+  for(int i=0; i<128*160; i++){
+    Send_Data(color >> 8); //Upper byte
+    Send_Data(color & 0xFF); //Lower byte
   }
 }
 
-
-
 //BGR color order
-
-
 void black()
 {
   SPI_SEND(0);
@@ -175,7 +137,6 @@ void setColor(int B, int G, int R)
   SPI_SEND(R);
 }
 
-
 void sprite(int XS, int XE, int YS, int YE, uint16_t color565)
 {
   Send_Command(CASET); // Set column range
@@ -195,15 +156,23 @@ void sprite(int XS, int XE, int YS, int YE, uint16_t color565)
     Send_Data(color565 & 0xFF);   // Low byte
   }
 }
+
 uint16_t Color565(uint8_t r, uint8_t g, uint8_t b) {
   return ((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3);
 }
 
+void enemySprite1 (int startX)
+{
+  int XS = startX;
+  int XE = startX + 9;
+  int YS = 0;
+  int YE = 9;
 
+  uint16_t blueColor = Color565(0, 0, 255); // bright blue
 
-
-
-
+  // Draw the enemy sprite
+  sprite(XS, XE, YS, YE, blueColor);
+}
 
 //Task struct for concurrent synchSMs implmentations
 typedef struct _task{
@@ -213,21 +182,12 @@ typedef struct _task{
 	int (*TickFct)(int); 		//Task tick function
 } task;
 
-
-
 const unsigned long JOYSTICK_PERIOD = 100;
 const unsigned long DISPLAY_PERIOD = 100;
 const unsigned long GCD_PERIOD = 1;
 
-
 #define NUM_TASKS 2
-
 task tasks[NUM_TASKS];
-
-
-
-
-//void TimerISR() {}
 
 void TimerISR() {
 	for ( unsigned int i = 0; i < NUM_TASKS; i++ ) {                   // Iterate through each task in the task array
@@ -238,11 +198,6 @@ void TimerISR() {
 		tasks[i].elapsedTime += GCD_PERIOD;                        // Increment the elapsed time by GCD_PERIOD
 	}
 }
-
-
-
-
-unsigned short xAxis;
 
 enum joystick{joystickInit, joystickRead};
 
@@ -259,184 +214,126 @@ int joystick(int state)
     break;
 
     default:
-      state = joystickInit;
+      state = joystickRead;
     break;
   }
 
   switch(state)
   {
     case joystickInit:
-      xAxis = 0;
     break;
 
-    //A0-PF0
     case joystickRead:
-      xAxis = ADC_read(0);
+      p1Joy = ADC_read(0);
+      Serial.println(p1Joy);
     break;
   }
 
   return state;
 }
 
+// Global sprite properties
+unsigned char playerXS = 54;   // Start X (centered)
+unsigned char playerXE = 74;   // End X (20 pixels wide)
+unsigned char playerYS = 140;  // Bottom of screen
+unsigned char playerYE = 159;  // 20 pixels tall
 
+int newXS = 0;
+int newXE = 0;
 
-unsigned char lastspriteX = 0;
-enum display{displayInit, displayUpdate};
+// Joystick X-axis value (updated in a separate task)
+//unsigned short xAxis;
 
-int display(int state)
-{
-  static unsigned char currentX = 0;
-  unsigned char newX;
+enum display { displayInit, displayMove };
 
+int display(int state) {
+    unsigned short threshold = 100;   // Dead zone
+    unsigned char stepSize = 10;       // Speed of movement
 
-  switch(state)
-  {
-    case displayInit:
-      state = displayUpdate;
-    break;
+    // State transitions
+    switch (state) {
+        case displayInit:
+            state = displayMove;
+            break;
 
-    case displayUpdate:
-      state = displayUpdate;
-    break;
+        case displayMove:
+            state = displayMove;
+            break;
 
-    default:
-      state = displayInit;
-    break;
-  }
+        default:
+            state = displayInit;
+            break;
+    }
 
+    // State actions
+    unsigned short xAxis = 0;
+    switch (state) 
+    {
+      case displayInit:
+        // Draw sprite for the first time
+        sprite(playerXS, playerXE, playerYS, playerYE, 0xF800); // red
+        break;
 
-  switch(state)
-  {
-    case displayInit:
-      lastspriteX = 0;
-    break;
+      case displayMove:
+        // Erase old sprite
+        sprite(playerXS, playerXE, playerYS, playerYE, 0x0000); // black
 
-    case displayUpdate:
-      newX = (xAxis * (128 - 40)) / 1023;
-      if (newX > 88) newX = 88;
-      sprite(lastspriteX, lastspriteX + 39, 140, 159, Color565(0, 255, 0));
-      sprite(newX, newX + 39, 140, 159, Color565(255, 0, 0));
-      lastspriteX = newX;
-    break;
-  }
+        // Move LEFT
+        if (xAxis < 512 - threshold) 
+        {
+          newXS = (int)playerXS - stepSize;
+          newXE = (int)playerXE - stepSize;
+
+          if (newXS >= 0) 
+          {
+            playerXS = (unsigned char)newXS;
+            playerXE = (unsigned char)newXE;
+          } 
+          else 
+          {
+            playerXS = 0;
+            playerXE = 20;
+          }
+        }
+
+        // Move RIGHT
+        else if (xAxis > 512 + threshold) 
+        {
+          newXS = (int)playerXS + stepSize;
+          newXE = (int)playerXE + stepSize;
+
+          if (newXE <= 127) 
+          {
+            playerXS = (unsigned char)newXS;
+            playerXE = (unsigned char)newXE;
+          } 
+          else 
+          {
+            playerXE = 127;
+            playerXS = 127 - 20;
+          }
+        }
+
+            // Draw sprite at new position
+        sprite(playerXS, playerXE, playerYS, playerYE, 0xF800); // red
+      break;
+
+      default:
+      break;
+    }
+
   return state;
 }
 
-
-// #define DISPLAY_WIDTH 128
-// #define SPRITE_WIDTH 20
-
-// enum displayStates { displayInit, displayLeft, displayRight };
-
-// int display(int state)
-// {
-//   static unsigned char oldX = 0;
-//   static unsigned char enteredLeft = 0;
-//   static unsigned char prevState = displayInit;
-//   unsigned char mappedX;
-
-//   // --- Joystick Mapping (manual, inverted, rounded) ---
-//   mappedX = ((1023 - xAxis) * (DISPLAY_WIDTH - SPRITE_WIDTH) + 511) / 1023;
-
-//   // Clamp
-//   if (mappedX > DISPLAY_WIDTH - SPRITE_WIDTH)
-//     mappedX = DISPLAY_WIDTH - SPRITE_WIDTH;
-
-//   // Optional edge fix: force right edge if joystick is near full up
-//   if (xAxis < 50)
-//     mappedX = DISPLAY_WIDTH - SPRITE_WIDTH;
-
-//   // --- State Transitions ---
-//   switch (state)
-//   {
-//     case displayInit:
-//       oldX = mappedX;
-//       state = displayRight;
-//       break;
-
-//     case displayLeft:
-//     case displayRight:
-//       if (xAxis < 400) {
-//         if (state != displayRight) {
-//           // (Optional) logic entering displayRight
-//         }
-//         prevState = state;
-//         state = displayRight;
-//       }
-//       else if (xAxis > 600) {
-//         if (state != displayLeft) {
-//           enteredLeft = 1; // One-time effect flag
-//         }
-//         prevState = state;
-//         state = displayLeft;
-//       }
-//       break;
-
-//     default:
-//       state = displayInit;
-//       break;
-//   }
-
-//   // --- State Actions ---
-//   switch (state)
-//   {
-//     case displayInit:
-//       sprite(mappedX, mappedX + SPRITE_WIDTH - 1, 140, 159, Color565(255, 0, 0));
-//       oldX = mappedX;
-//       break;
-
-//     case displayLeft:
-//       if (mappedX != oldX)
-//       {
-//         // Erase previous sprite
-//         sprite(oldX, oldX + SPRITE_WIDTH - 1, 140, 159, Color565(0, 255, 0));
-
-//         // Draw red sprite
-//         sprite(mappedX, mappedX + SPRITE_WIDTH - 1, 140, 159, Color565(255, 0, 0));
-
-//         oldX = mappedX;
-//       }
-//       break;
-
-//     case displayRight:
-//       if (mappedX != oldX)
-//       {
-//         // Erase previous sprite
-//         sprite(oldX, oldX + SPRITE_WIDTH - 1, 140, 159, Color565(0, 255, 0));
-
-//         // Draw red sprite
-//         sprite(mappedX, mappedX + SPRITE_WIDTH - 1, 140, 159, Color565(255, 0, 0));
-
-//         oldX = mappedX;
-//       }
-//       break;
-//   }
-
-//   return state;
-// }
-
-
-
+// need to create the objects falling 
+// 4 objects will fall at random and will be roughly 10*10 to test
+// object 1: bullet (5*5)
+// object 2: sword (7*7)
+// object 3: vase (6*6)
+// object 4: plane (25*25)
 
 int main(void)
 {
-
-  // //declare ports
-  // DDRF = 0x00;
-  // PORTF = 0x01;
-
-  // DDRB = 0x06;
-  // PORTB = 0x00;
-
-  // DDRH = 0x08;
-  // PORTH = 0x00;
-
-  // DDRG = 0x20;
-  // PORTG = 0x00;
-
-  // DDRE = 0x20;
-  // PORTE = 0x00;
-
   DDRB = 0xFF;
   PORTB = 0x00;
 
@@ -449,19 +346,13 @@ int main(void)
   DDRE = 0xFF;
   PORTE = 0x00;
 
-
-
   ADC_init();
   SPI_INIT();
   ST7735_init();
-  //_delay_ms(100);
-  blackbackGround(0x07E0);
-  //sprite(20, 59, 30, 69, Color565(255, 0, 0));  // red
-  
-  unsigned char i = 0;
+  fillScreen(0x0000, 0x0000);
+  enemySprite1(60);
 
-  
-  
+  unsigned char i = 0;
   tasks[0].period = JOYSTICK_PERIOD;
   tasks[0].state = joystickInit;
   tasks[0].elapsedTime = JOYSTICK_PERIOD;
@@ -473,12 +364,12 @@ int main(void)
   tasks[1].TickFct = &display;
   i++;
 
-
-
   TimerSet(GCD_PERIOD);
   TimerOn();
 
-  while(1){}
+  while(1){
+    //Serial.println(p1Joy);
+  }
 
   return 0;
 }
